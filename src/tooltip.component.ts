eb045d69bb9a13f853d273c41f719b7a6bd8feca
Tyler Howard <thowar4@students.towson.edu>
2018-03-28 15:34:24 -0400

    Add left and bottom positions. Fix positioning bug Closes #3. Closes #4.

--- src/tooltip.component.ts
+++ src/tooltip.component.ts
@@ -25,7 +25,7 @@ export class TooltipComponent implements AfterViewInit, OnInit {
   constructor(private elementRef: ElementRef, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(){
-    this.location ? this.location = this.location : this.location = "side"
+    this.location ? this.location = this.location : this.location = "right"
     this.assignClass(this.location);
   }
 
@@ -48,36 +48,52 @@ export class TooltipComponent implements AfterViewInit, OnInit {
   * @Input location as string
   * Checks location input and if valid, assigns corresponding class for styling
   */
+  // TODO: Deprecate side and above tags
+
   private assignClass(location: string){
-    if (location ==="side"){
+    if (location ==="right" || location === "side"){
       this.class = "right";
-    } else if (location === "above"){
+    } else if (location === "left"){
+      this.class = "left";
+    } else if (location === "top" || location === "above" ) {
       this.class = "top";
-    } else {
-      console.log("Bad location for tooltip! Try 'above' or 'side'.")
+    } else if (location === "bottom") {
+      this.class = "bottom";
+    }
+    else {
+      console.log("Bad location for tooltip! Try 'top', 'bottom', 'right', or 'left'.")
     }
   }
-  
+
   /*
   * @Input location as string
   * Calculates position on screen according to selected placement.
   */
+   // TODO: Deprecate side and above tags
   private calculatePosition(location: string) {
     let x = -999;
     let y = -999;
-    const offsetConst = 10;
+    const offsetConst = 5;
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
-    let scrollY = window.pageYOffset;
+    let offsetY = window.pageYOffset;
+    let scrollY = window.scrollY || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
     
-    if (location === "side"){
-      x = this.parent.getBoundingClientRect().left + this.parent.offsetWidth + offsetConst;
-      y = (this.parent.getBoundingClientRect().top + scrollY) + this.parent.offsetHeight / 2 - this.el.clientHeight / 2;
+    if (location === "right" || location === "side"){
+      x = this.parent.getBoundingClientRect().left + (this.parent.offsetWidth + offsetConst);
+      y = (this.parent.getBoundingClientRect().top + offsetY - scrollY) + (this.parent.offsetHeight / 2) - (this.el.offsetHeight / 2);
     } 
-    // TODO: Need to correct offset in this math to display better
-    else if (location === "above"){
-      x = this.parent.getBoundingClientRect().left + this.parent.offsetWidth / 2 - this.el.clientWidth / 2;
-      y = (this.parent.getBoundingClientRect().top + scrollY) - this.parent.offsetHeight - this.el.offsetHeight/2;
+    else if (location === "left") {
+      x = this.parent.getBoundingClientRect().right - (this.parent.offsetWidth + this.el.offsetWidth + offsetConst);
+      y = (this.parent.getBoundingClientRect().top + offsetY - scrollY) + (this.parent.offsetHeight / 2) - (this.el.offsetHeight / 2);
+    } 
+    else if (location === "top" || location === "above"){
+      x = this.parent.getBoundingClientRect().left + (this.parent.offsetWidth / 2) - (this.el.offsetWidth / 2);
+      y = (this.parent.getBoundingClientRect().bottom + offsetY - scrollY) - (this.parent.offsetHeight + this.el.offsetHeight + offsetConst);
+    }
+    else if (location === "bottom") {
+      x = this.parent.getBoundingClientRect().left + (this.parent.offsetWidth / 2) - (this.el.offsetWidth / 2);
+      y = (this.parent.getBoundingClientRect().top + offsetY - scrollY) + (this.parent.offsetHeight + offsetConst);
     }
     this.assignCoords(x, y);
   }
